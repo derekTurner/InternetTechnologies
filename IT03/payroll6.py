8f0fd12bd0fb91ee788419dac344321110c85d8f
Derek Turner <derek.turner@uws.ac.uk>
2018-08-16 13:20:25 +0100

    at step 6

---
+++ IT03/payroll6.py
@@ -0,0 +1,58 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class myHandler(BaseHTTPRequestHandler):
+
+    def Send200(self, mess):
+
+        try:
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes(mess, 'utf-8'))
+        except ConnectionResetError:
+            pass
+
+    def SendError(self, status, mess):
+
+        try:
+            self.send_response(status)
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(bytes(mess, 'utf-8'))
+        except ConnectionResetError:
+            pass
+
+    def do_GET(self):
+
+        pp = self.path.split('/')
+        if len(pp) < 3:
+            self.Send200(self.path + ' is too short')
+        elif pp[2] == 'Employee':
+            self.Send200('Employee request')
+        elif pp[2] == 'Post':
+            self.Send200('Post request')
+        elif pp[2] == 'Holiday':
+            self.Send200('Holiday request')
+        else:
+            self.SendError(400, 'Expected one of Employee/,Post/,Holiday/')
+
+    def GetData(self):
+        if not self.headers.__contains__('Content-Length'):
+            return None
+        h = self.headers.__getitem__('Content-Length')
+        n = int(h)
+        return str(self.rfile.read(n), 'utf-8')
+
+    def do_POST(self):
+        try:
+            s = self.GetData()
+            self.Send200(s + ' was posted')
+        except Exception as e:
+            self.SendError(500, repr(e))
+        return
+
+
+server = HTTPServer(('', 8088), myHandler)
+print('Started httpserver')
+server.serve_forever()
